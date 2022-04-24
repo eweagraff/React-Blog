945c36db5a75ca062c060cd18a9cfce6a33eae1b
Emily Weagraff <emilyweagraff@Emilys-MBP-2.attlocal.net>
2022-04-23 22:39:02 -0700

    removed embedded clip from home.js

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ return (
         <div className="home">
             {error && <div> {error}</div>}
            {blogs && <Bloglist blogs={blogs} title="Current Tips"/>}
-           <iframe width ="420" height ="315"title ="video"src="https://www.youtube.com/embed/MBkYD2HLy34"></iframe>
+         
         </div>
         
     )};
